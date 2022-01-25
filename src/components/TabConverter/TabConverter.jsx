2a6bf768efc437ff0108a2c02a7777d5c01b910f
hoonjoo94 <hoonjoo94@naver.com>
2022-01-26 03:37:39 +0900

    ♻️ API키 사용만료로 교체, 스타일링 수정, 환율 계산 코드 리팩토링

--- src/components/TabConverter/TabConverter.jsx
+++ src/components/TabConverter/TabConverter.jsx
@@ -1,21 +1,19 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { FormBox } from "./FormBox";
-import { ResultBox } from "./ResultBox";
-import { API_ENDPOINT, TAB_CURRENCY } from "../../constants";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { FormBox } from './FormBox';
+import { ResultBox } from './ResultBox';
+import { API_ENDPOINT, TAB_CURRENCY } from '../../constants';
 
 export const TabConverter = () => {
   const [apiData, setApiData] = useState({});
-  const [currentTab, setCurrentTab] = useState("");
-  const [currency, setCurrency] = useState("USD");
-  const [inputValue, setInputValue] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+  const [currentTab, setCurrentTab] = useState('');
+  const [currency, setCurrency] = useState('USD');
+  const [inputValue, setInputValue] = useState('');
   useEffect(() => {
-    fetch(API_ENDPOINT(TAB_CURRENCY.join(",")))
+    fetch(API_ENDPOINT(TAB_CURRENCY.join(',')))
       .then((res) => res.json())
       .then((data) => {
         setApiData({ quotes: data.quotes, date: data.timestamp });
-        setIsLoading(false);
       });
   }, [currency]);
 
@@ -32,7 +30,6 @@ export const TabConverter = () => {
         currency={currency}
         apiData={apiData}
         inputValue={inputValue}
-        isLoading={isLoading}
       />
     </TabConverterContainer>
   );
@@ -44,4 +41,6 @@ const TabConverterContainer = styled.div`
   padding: 30px;
   border: 5px solid #000;
   border-radius: 15px;
+  background-color: #fff;
+  box-shadow: 0px 3px 7px 1px rgba(0, 0, 0, 0.75);
 `;
