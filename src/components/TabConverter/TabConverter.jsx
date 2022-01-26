df0d6cdc0fe8e8faff547ba1c3517a57db27653c
Joy Lee <joylee.dev@gmail.com>
2022-01-26 10:35:13 +0900

    💄 API 로딩 실패 시 UI 추가

--- src/components/TabConverter/TabConverter.jsx
+++ src/components/TabConverter/TabConverter.jsx
@@ -5,31 +5,31 @@ import { ResultBox } from './ResultBox';
 import { API_ENDPOINT, TAB_CURRENCY } from '../../constants';
 
 export const TabConverter = () => {
-  const [apiData, setApiData] = useState({});
+  const [apiData, setApiData] = useState({ quotes: '', date: '' });
   const [currentTab, setCurrentTab] = useState('');
   const [currency, setCurrency] = useState('USD');
   const [inputValue, setInputValue] = useState('');
+  const [isLoading, setIsLoading] = useState('true');
+
   useEffect(() => {
     fetch(API_ENDPOINT(TAB_CURRENCY.join(',')))
       .then((res) => res.json())
       .then((data) => {
-        setApiData({ quotes: data.quotes, date: data.timestamp });
+        setIsLoading(false);
+        if (data.quotes) setApiData({ quotes: data.quotes, date: data.timestamp });
       });
   }, [currency]);
 
   return (
     <TabConverterContainer>
-      <FormBox
-        setCurrency={setCurrency}
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-      />
+      <FormBox setCurrency={setCurrency} inputValue={inputValue} setInputValue={setInputValue} />
       <ResultBox
         currentTab={currentTab}
         setCurrentTab={setCurrentTab}
         currency={currency}
         apiData={apiData}
         inputValue={inputValue}
+        isLoading={isLoading}
       />
     </TabConverterContainer>
   );
